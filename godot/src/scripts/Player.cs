09ca3f912f17ef3baf06e7a1c2d07d5236cc0318
MichaelR <michael.ratzel@dlr.de>
2019-05-15 11:51:34 +0200

    Changed gravity to be able to use custom integrator

--- godot/src/scripts/Player.cs
+++ godot/src/scripts/Player.cs
@@ -8,10 +8,14 @@ public class Player : RigidBody2D
     [Export] public int forceRight = 10000;
     [Export] public int forceLeft = 10000;
 
+    [Export] public int gravityAccel = 5000;
+
     [Export] public int speedJump = 2000;
+    [Export] public int physicFramesForJump = 4;
 
     private Vector2 initPosition;
-    private bool jumpNextPhysicsFrame;
+    private int physicFramesSinceJumpEvent;
+    private bool wantToJump;
 
     public override void _Ready()
     {
@@ -29,13 +33,13 @@ public class Player : RigidBody2D
         //*************************************************
 
         // Player will be slowed more if he touches a floor or wall, smaller factor more slowing
-        float xSlowFactor = (floorWallInt == -1) ? 0.92f : 0.85f;
+        float xSlowFactor = (floorWallInt == -1) ? 0.98f : 0.85f;
         // Player can't accelerate as quick if he doesn't touch a floor or wall
-        float xAccelFactor = (floorWallInt == -1) ? 0.4f : 1.0f;
+        float xForceFactor = (floorWallInt == -1) ? 0.2f : 1.0f;
 
         // Final x-Velocity is the old x-Velocity multiplied with the slowFactor and added with the velocity from the inputs
         // The inputs create an force which is converted by the Player Mass and the needed time of this physicsProcess frame into a velocity
-        Vector2 finalVel = new Vector2(xSlowFactor, 1) * state.GetLinearVelocity() + xAccelFactor * getInputForceLeftRight() * state.GetInverseMass() * GetPhysicsProcessDeltaTime();
+        Vector2 finalVel = new Vector2(xSlowFactor, 1) * state.GetLinearVelocity() + xForceFactor * getInputForceLeftRight() * state.GetInverseMass() * GetPhysicsProcessDeltaTime();
 
         // Clamp the final x-Velocity to the defined max Speeds. 
         // Maybe use the old x-Velocity multiplied with the slowFactor, to allow higher x-Velocities for the player from other sources
@@ -45,24 +49,62 @@ public class Player : RigidBody2D
 
 
         //*************************************************
+        // Gravity
+        //*************************************************
+        finalVel = addGravityToVelocity(state, finalVel, floorWallInt);
+
+        //*************************************************
         // Jump Movement
         //*************************************************
-
-        if (floorWallInt != -1 && jumpNextPhysicsFrame)
-        {
-            finalVel.y = 0;
-            finalVel += getJumpVelocityFromFloorWallNormal(state.GetContactLocalNormal(floorWallInt));
-        }
-        jumpNextPhysicsFrame = false;
+        finalVel = addJumpEventToVelocity(state, finalVel, floorWallInt);
 
         state.SetLinearVelocity(finalVel);
+    }
+
+    private Vector2 addGravityToVelocity(Physics2DDirectBodyState state, Vector2 inputVelocity, int floorWallInt)
+    {
+        float gravityFactor = 1.0f;
+        if (floorWallInt != -1)
+        {
+            Vector2 floorNormal = state.GetContactLocalNormal(floorWallInt).Normalized();
+            gravityFactor = 1.0f - Mathf.Max(floorNormal.Dot(Vector2.Up), 0.0f);
+        }
+
+        inputVelocity.y += gravityFactor * gravityAccel * GetPhysicsProcessDeltaTime();
+        return inputVelocity;
+
+    }
+
+    private Vector2 addJumpEventToVelocity(Physics2DDirectBodyState state, Vector2 inputVelocity, int floorWallInt)
+    {
+        // After a while the player doesn't want to jump anymore
+        if(physicFramesSinceJumpEvent > physicFramesForJump)
+        {
+            wantToJump = false;
+        }
+        ++physicFramesSinceJumpEvent;
+
+        // If touching a floorWall and the player still wants to jump -> (s)he'll jump
+        if (floorWallInt != -1 && wantToJump)
+        {
+            inputVelocity.y = 0;
+            inputVelocity += getJumpVelocityFromFloorWallNormal(state.GetContactLocalNormal(floorWallInt));
+            wantToJump = false;
+        }
+
+        return inputVelocity;
+
     }
 
     public override void _Input(InputEvent @event)
     {
         // The jump input event shall only be triggered once by pushing the button not permanently by holding the butten
         // The event must be checked here to achieve the desired behavior
-        jumpNextPhysicsFrame = @event.IsActionPressed("game_jump");
+        if (@event.IsActionPressed("game_jump"))
+        {
+            wantToJump = true;
+            physicFramesSinceJumpEvent = 0;
+        }
 
         base._Input(@event);
     }
@@ -76,7 +118,7 @@ public class Player : RigidBody2D
         {
             if ((Physics2DServer.BodyGetCollisionLayer(state.GetContactCollider(i)) & LayerNames.Physics2D.floorWall) != 0)
             {
-                float currentAngleToUp_abs = Mathf.Abs(state.GetContactLocalNormal(i).AngleTo(new Vector2(0, -1)));
+                float currentAngleToUp_abs = Mathf.Abs(state.GetContactLocalNormal(i).AngleTo(Vector2.Up));
                 if (currentAngleToUp_abs < angleToUpVector_abs)
                 {
                     floorWall = i;
@@ -107,7 +149,11 @@ public class Player : RigidBody2D
     // Get the jump velocity. The direction is the perpendicular bisector of the surface normal and the up vector
     private Vector2 getJumpVelocityFromFloorWallNormal(Vector2 normal)
     {
-        Vector2 jumpDirection = normal.Normalized() + new Vector2(0, -1);
+        Vector2 jumpDirection = normal.Normalized() + Vector2.Up;
+        if (jumpDirection.Equals(Vector2.Zero))
+        {
+            return Vector2.Zero;
+        }
         return jumpDirection.Normalized() * speedJump;
     }
 }
