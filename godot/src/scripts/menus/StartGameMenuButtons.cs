89e8091a578be4ff43b52d57f2e05b2a049203c2
Rebekka Litzelmann <rebekka.litzelmann@web.de>
2019-05-21 10:06:22 +0200

    Solve discussion on #2 Create base gui. Delete comments and Using System

--- godot/src/scripts/menus/StartGameMenuButtons.cs
+++ godot/src/scripts/menus/StartGameMenuButtons.cs
@@ -1,34 +1,21 @@
 using Godot;
-using System;
 
 public class StartGameMenuButtons : VBoxContainer
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 	private void _on_toStartMenu_pressed()
-		{
-		    GD.Print("Back to da roots ma friend; aka to StartMenu");
-			GetTree().ChangeScene("res://src/scenes/menus/StartMenuScene.tscn");
-		}
-		
-		
-		private void _on_StartGame_pressed()
-		{
-		    GD.Print("Start Game!");
-		    GetTree().ChangeScene("res://src/scenes/InGameScene.tscn");
-		}
+	{
+	    GD.Print("Back to da roots ma friend; aka to StartMenu");
+		GetTree().ChangeScene("res://src/scenes/menus/StartMenuScene.tscn");
+	}
 
+	private void _on_StartGame_pressed()
+	{
+	    GD.Print("Start Game!");
+	    GetTree().ChangeScene("res://src/scenes/InGameScene.tscn");
+	}
 }
--- godot/src/scripts/menus/StartMenuButtons.cs
+++ godot/src/scripts/menus/StartMenuButtons.cs
@@ -1,34 +1,22 @@
 using Godot;
-using System;
 
 public class StartMenuButtons : VBoxContainer
 {
     [Export] NodePath quitDialog;
     private ConfirmationDialog myQuitDialog;
 
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+  
     public override void _Ready()
     {
         myQuitDialog = GetNode(quitDialog) as ConfirmationDialog;
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
     private void _on_StartGame_pressed()
     {
-	    //Print, not print !!!!!!!!!!afanafsnvsa12121111elf
 	    GD.Print("Start Game!");
         GetTree().ChangeScene("res://src/scenes/menus/StartGameMenuScene.tscn");
     }
-
 
     private void _on_Options_pressed()
     {
@@ -36,18 +24,15 @@ public class StartMenuButtons : VBoxContainer
         GetTree().ChangeScene("res://src/scenes/menus/OptionsMenuScene.tscn");
     }
 
-
     private void _on_Quit_pressed()
     {
         GD.Print("Quit? Popup");
         myQuitDialog.PopupCentered(new Vector2(300,150));
     }
 
-
     private void _on_myQuitDialog_confirmed()
     {
         GD.Print("Quit!");
         GetTree().Quit();
     }
-
 }
