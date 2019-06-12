3a484913f6b3351611a9ec8b3f7e737f08bc6c95
Jonathan Fritsch <jdfritsch76@gmail.com>
2019-06-12 19:12:04 +0200

    Improve and document Healthkit

--- godot/src/scripts/HealthKit.cs
+++ godot/src/scripts/HealthKit.cs
@@ -1,10 +1,16 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Healthkit SupplyObject that restores a variable amount of health to the player.
+/// </summary>
 public class HealthKit : SupplyObject
 {
+    [Export]
+    public int amount;
+
     protected override void DoEffect(Player target)
 	{
-		target.Health += 10;
+		target.Health += amount;
 	}
 }
