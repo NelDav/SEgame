13089e7c746898bf64fa3f422339f94898eb0ce3
NelDav <david.nelles@gmx.de>
2019-06-23 14:51:10 +0200

    Added ammunitionKit

---
+++ godot/src/scripts/supplyobjects/AmmunitionKit.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using System;
+
+/// <summary>
+/// Healthkit SupplyObject that restores a variable amount of health to the player.
+/// </summary>
+public class AmmunitionKit : SupplyObject
+{
+    [Export]
+    public int amount;
+
+    protected override void DoEffect(Player target)
+    {
+        if(target.CurrentWeapon is RangedWeapon)
+        {
+            ((RangedWeapon)target.CurrentWeapon).CurrentAmmunition += amount;
+        }
+    }
+}
